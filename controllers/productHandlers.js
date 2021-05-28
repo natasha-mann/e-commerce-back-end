b556e4c103e8c94f99953e5354c76544d4fe8251
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-28 22:15:56 +0100

    WIP createProduct handler

--- controllers/productHandlers.js
+++ controllers/productHandlers.js
@@ -39,7 +39,7 @@ const getProduct = async (req, res) => {
   }
 };
 
-const createProduct = (req, res) => {
+const createProduct = async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,26 +48,35 @@ const createProduct = (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  try {
+    const { id, product_name, price, stock, tagIds } = await Product.create(
+      req.body
+    );
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: id,
+          tag_id,
+        };
+      });
+      const productTagIds = await ProductTag.bulkCreate(productTagIdArr);
+      const newObject = {
+        product_name,
+        price,
+        stock,
+        tagIds,
+        ...productTagIds,
+      };
+      console.log(newObject);
+      res.status(200).json(newObject);
+    } else {
+      console.log("NEW", newProduct);
+      res.status(200).json(newProduct);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
 };
 
 const updateProduct = (req, res) => {
