5bb387f565837592385381402b438c59f7b4cb38
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-28 14:08:50 +0100

    added columns for category model

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  categoryName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "category",
+};
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+Category.init(schema, options);
 
 module.exports = Category;
