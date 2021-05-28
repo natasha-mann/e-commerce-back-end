2302c8affb5dd74cada140bad884042e74ebd3a5
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-28 14:23:03 +0100

    WIP productTag and Tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,26 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product_tag",
+};
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+ProductTag.init(schema, options);
 
 module.exports = ProductTag;
--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tagName: {
+    type: DataTypes.STRING,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "tag",
+};
 
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
-);
+Tag.init(schema, options);
 
 module.exports = Tag;
