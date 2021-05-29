1e09ef96c95bd98c59203d90ddbb2f9f3799fc32
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-29 13:36:26 +0100

    updated association links

--- src/models/index.js
+++ src/models/index.js
@@ -4,9 +4,13 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 Product.belongsToMany(Tag, { through: ProductTag });
 
