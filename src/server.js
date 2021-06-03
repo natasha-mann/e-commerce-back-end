329f1a233c9fa6e1fd4518c22c54c7fdc03ecaf5
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-03 16:43:50 +0100

    added custom logger middleware

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 
 const routes = require("./routes");
-// import sequelize connection
+const logger = require("./middlewares/logger");
+
 const connection = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 app.use(routes);
 
