8e75a8d55ee6678f1c00aedf771174dd0337bb0e
Suraj Verma <surajverma@rentalcars.com>
2021-07-04 14:12:29 +0100

    refactor category controller

--- src/controllers/api/categories.js
+++ src/controllers/api/categories.js
@@ -1,42 +1,44 @@
 const { Category, Product } = require("../../models");
 
+const PRODUCT_ATTRIBUTES = ["product_name", "price", "stock"];
+
+const handleError = (req, res, message, error = {}, statusCode = 500) => {
+  if (statusCode === 500) {
+    console.error(`ERROR | ${req.method} | ${req.baseUrl} | ${error.message}`);
+    return res.status(500).json({ error: message });
+  } else {
+    console.error(`ERROR | ${req.method} | ${req.baseUrl} | ${message}`);
+    return res.status(statusCode).json({
+      error: message,
+    });
+  }
+};
+
 const getAllCategories = async (req, res) => {
   try {
-    const allCategories = await Category.findAll({
-      include: [
-        { model: Product, attributes: ["product_name", "price", "stock"] },
-      ],
+    const categories = await Category.findAll({
+      include: [{ model: Product, attributes: PRODUCT_ATTRIBUTES }],
     });
-    res.json(allCategories);
-  } catch (err) {
-    console.log(`[ERROR]: ${err.message}`);
-    res.status(500).json({
-      error: "Failed to get categories",
-    });
+
+    return res.json(categories);
+  } catch (error) {
+    return handleError(req, res, "Failed to get categories", error);
   }
 };
 
 const getCategory = async (req, res) => {
   try {
     const category = await Category.findByPk(req.params.id, {
-      include: [
-        { model: Product, attributes: ["product_name", "price", "stock"] },
-      ],
+      include: [{ model: Product, attributes: PRODUCT_ATTRIBUTES }],
     });
 
     if (!category) {
-      console.log(`[INVALID ID]: Unable to get category by ID`);
-      return res.status(404).json({
-        error: "Category does not exist",
-      });
+      return handleError(req, res, "Category does not exist", undefined, 404);
     }
 
-    res.json(category);
-  } catch (err) {
-    console.log(`[ERROR]: ${err.message}`);
-    res.status(500).json({
-      error: "Failed to get category",
-    });
+    return res.json(category);
+  } catch (error) {
+    return handleError(req, res, "Failed to get category", error);
   }
 };
 
@@ -45,82 +47,81 @@ const createCategory = async (req, res) => {
     const { category_name } = req.body;
 
     if (!category_name) {
-      console.log(`[ERROR]: Unable to create a category`);
-      return res.status(404).json({
-        error: "Unable to create a category",
-      });
+      return handleError(
+        req,
+        res,
+        "Unable to create a category",
+        undefined,
+        400
+      );
     }
 
     const newCategory = await Category.create(req.body);
-    res.json(newCategory);
+    return res.json(newCategory);
   } catch (error) {
-    console.log(`[ERROR]: ${error.message}`);
-    res.status(500).json({
-      error: "Failed to create category",
-    });
+    return handleError(req, res, "Failed to create category", error);
   }
 };
 
 const updateCategory = async (req, res) => {
   try {
-    const category = await Category.findByPk(req.params.id);
     const { category_name } = req.body;
+    const { id } = req.params;
 
-    // if the category trying to be updated doesn't exist
-    if (!category) {
-      console.log(`[INVALID CATEGORY ID]: Unable to update the category.`);
-      return res.status(404).json({
-        error: "Category does not exist.",
-      });
+    if (!category_name) {
+      return handleError(
+        req,
+        res,
+        "Unable to update the category",
+        undefined,
+        400
+      );
     }
 
-    // if the request body doesn't contain new category information
-    if (!category_name) {
-      console.log(`[ERROR]: Unable to update the category`);
-      return res.status(404).json({
-        error: "Unable to update the category",
-      });
+    const category = await Category.findByPk(id);
+
+    if (!category) {
+      return handleError(
+        req,
+        res,
+        "Unable to update the category",
+        undefined,
+        404
+      );
     }
 
     const newCategory = await Category.update(
       { category_name },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
+      { where: { id } }
     );
-    res.json(newCategory);
+
+    return res.json({ success: !!newCategory });
   } catch (error) {
-    console.log(`[ERROR]: ${error.message}`);
-    res.status(500).json({
-      error: "Failed to update category",
-    });
+    return handleError(req, res, "Failed to update category", error);
   }
 };
 
 const deleteCategory = async (req, res) => {
   try {
-    const category = await Category.findByPk(req.params.id);
+    const { id } = req.params;
+
+    const category = await Category.findByPk(id);
 
     if (!category) {
-      console.log(`[ERROR]: Unable to delete the category`);
-      return res.status(404).json({
-        error: "Unable to delete the category",
-      });
+      return handleError(
+        req,
+        res,
+        "Unable to delete the category",
+        undefined,
+        404
+      );
     }
 
-    const newCategory = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.json(newCategory);
+    const newCategory = await Category.destroy({ where: { id } });
+
+    return res.json({ success: !!newCategory });
   } catch (error) {
-    console.log(`[ERROR]: ${error.message}`);
-    res.status(500).json({
-      error: "Failed to delete category",
-    });
+    return handleError(req, res, "Failed to delete category", error);
   }
 };
 
